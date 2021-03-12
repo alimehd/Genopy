af28f68b58381508ffb6409a5846954d443392e2
valentinagoanta <69973334+valentinagoanta@users.noreply.github.com>
2021-03-12 21:42:14 +0200

    Update network.py

--- network.py
+++ network.py
@@ -240,3 +240,26 @@ class VaeGan(nn.Module):
         bce_dis_sampled = -torch.log(1 - disc_class_sampled + 1e-3)
 
         return nle, kl, mse, bce_dis_original, bce_dis_predicted, bce_dis_sampled
+    
+    #Create LSTM model
+    
+    class LSTMTagger(nn.Module):
+        def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
+            super(LSTMTagger, self).__init__()
+            self.hidden_dim = hidden_dim
+            
+            self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+
+        # The LSTM takes word embeddings as inputs, and outputs hidden state with dimensionality hidden_dim.
+            
+            self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+
+        # The linear layer that maps from hidden state space to tag space
+            self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+            
+         def forward(self, sentence):
+            embeds = self.word_embeddings(sentence)
+            lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
+            tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
+            tag_scores = F.log_softmax(tag_space, dim=1)
+            return tag_scores
